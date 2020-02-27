12021ab79add7328d61f71049d01431a5418d156
nicbongo <nicbongo@protonmail.com>
2020-02-27 14:05:57 -0400

    basic api tests

--- cohorts/router.js
+++ cohorts/router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.status(201).json({ router: "students" });
+  res.status(200).json({ router: "cohorts" });
 });
 
 module.exports = router;
